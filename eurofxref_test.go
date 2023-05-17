785d7c1c37036763d930f45b113035cd09c97912
Henrique Dias <mrhdias@gmail.com>
2023-05-17 22:16:05 +0100

    Update eurofxref_test.go

--- eurofxref_test.go
+++ eurofxref_test.go
@@ -2,7 +2,7 @@
 // Copyright 2023 The GoEurofxref Authors. All rights reserved.
 // Use of this source code is governed by a MIT License
 // license that can be found in the LICENSE file.
-// Last Modification: 2023-05-17 14:45:43
+// Last Modification: 2023-05-17 22:15:25
 //
 
 package eurofxref
@@ -23,6 +23,15 @@ func TestEuroFxRef(t *testing.T) {
 
 	if _, err := query.Daily("USD"); err != nil {
 		t.Fatal(err)
+	}
+
+	want := 1.00
+	got, err := query.Daily("EUR")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got.RateValue != want {
+		t.Errorf("got = %.2f, want %.2f", got.RateValue, want)
 	}
 
 	if _, err := os.Stat(cacheDir); !os.IsNotExist(err) {
